809bd4fefb65ee704852cd1709552fe0a8253d98
Iftekher Mahmud <iftekher.mahmud@nagad.com.bd>
2023-03-06 11:43:27 +0600

    3 Commit

--- src/main/java/com/atoms/gittest/model/TestModel.java
+++ src/main/java/com/atoms/gittest/model/TestModel.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 public class TestModel {
     private int id;
     private String string1;
-    private String string2;
+    private String string3;
     private double double1;
     private double double2;
 }
