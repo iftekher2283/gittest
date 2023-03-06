aea05d2ea7deb38abb31d802eea9061e7b79f08f
Iftekher Mahmud <iftekher.mahmud@nagad.com.bd>
2023-03-06 15:08:05 +0600

    Merge branch 'master' of https://github.com/iftekher2283/gittest
     Conflicts:
    	src/main/java/com/atoms/gittest/model/TestModel2.java

--- src/main/java/com/atoms/gittest/model/TestModel2.java
+++ src/main/java/com/atoms/gittest/model/TestModel2.java
@@ -1,9 +1,9 @@
 package com.atoms.gittest.model;
 
 public class TestModel2 {
-    private String s16;
+    private String s116;
     private String s6;
     private String s7;
-    private String s14;
+    private String s114;
     private String s5;
 }
