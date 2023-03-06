e8dacef28a0c94f0995e9241032a300926c09659
Iftekher Mahmud <iftekher.mahmud@nagad.com.bd>
2023-03-06 14:56:46 +0600

    Model 2 1

--- src/main/java/com/atoms/gittest/model/TestModel2.java
+++ src/main/java/com/atoms/gittest/model/TestModel2.java
@@ -1,9 +1,9 @@
 package com.atoms.gittest.model;
 
 public class TestModel2 {
-    private String s1;
+    private String s11;
     private String s6;
     private String s7;
-    private String s4;
+    private String s14;
     private String s5;
 }
