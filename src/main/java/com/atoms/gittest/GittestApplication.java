365bcf959928b166ec3626b1ef65e023732b3c56
Iftekher Mahmud <iftekher.mahmud@nagad.com.bd>
2023-03-06 11:39:48 +0600

    Initial commit

---
+++ src/main/java/com/atoms/gittest/GittestApplication.java
@@ -0,0 +1,16 @@
+package com.atoms.gittest;
+
+import com.atoms.gittest.model.TestModel;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class GittestApplication {
+
+	public static void main(String[] args) {
+		TestModel model = new TestModel();
+		System.out.println("Model ---> " + model);
+//		SpringApplication.run(GittestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/atoms/gittest/model/TestModel.java
@@ -0,0 +1,16 @@
+package com.atoms.gittest.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TestModel {
+    private int id;
+    private String string1;
+    private String string2;
+    private double double1;
+    private double double2;
+}
